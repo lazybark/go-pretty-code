652acfe9f17416c971273fa5c632025c6c2fc562
Sergey Krekhno <sergey.krekhno@gmail.com>
2022-04-20 23:29:46 +0300

    Console colors

---
+++ console/console.go
@@ -0,0 +1,13 @@
+package console
+
+var (
+	TextColorReset  = "\033[0m"
+	TextColorRed    = "\033[31m"
+	TextColorGreen  = "\033[32m"
+	TextColorYellow = "\033[33m"
+	TextColorBlue   = "\033[34m"
+	TextColorPurple = "\033[35m"
+	TextColorCyan   = "\033[36m"
+	TextColorGray   = "\033[37m"
+	TextColorWhite  = "\033[97m"
+)
